c62dffbd71634f9f112a90c3d254298b60ed7e73
Vladimir Katiurin <v.katiurin@catawiki.nl>
2023-12-13 00:48:17 +0100

    d09

---
+++ 2023/d09/9.2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"advent-of-code/utils"
+	"advent-of-code/utils/slices"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	isSample := flag.Bool("s", false, "read sample")
+	flag.Parse()
+
+	utils.Run(solve, utils.ReadInput(*isSample))
+}
+
+func solve(data string) {
+	var sum int
+	for _, row := range strings.Split(data, "\n") {
+		seq := slices.StringToIntSlice(row)
+
+		sum += Expand(seq)
+	}
+
+	fmt.Println(sum)
+}
+
+func Expand(seq []int) int {
+	allZero := true
+	for _, el := range seq {
+		if el != 0 {
+			allZero = false
+			break
+		}
+	}
+
+	if !allZero {
+		var newSeq []int
+		for i := 0; i < len(seq)-1; i++ {
+			newSeq = append(newSeq, seq[i+1]-seq[i])
+		}
+		return seq[0] - Expand(newSeq)
+	} else {
+		return 0
+	}
+}
